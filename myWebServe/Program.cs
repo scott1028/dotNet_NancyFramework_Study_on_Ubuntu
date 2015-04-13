d8a39aae0774ac9369a007ff8d7ebf9f095e6f05
scott.lan <scott.lan@taisys.com>
2015-04-13 15:44:53 +0800

    fm

---
+++ myWebServe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using System.Threading;
+using System.Linq;
+
+/*
+ * ref: http://nancyfx.org/
+ * ref: https://github.com/NancyFx/Nancy/wiki/Hosting-Nancy-with-Nginx-on-Ubuntu#create-nancy-website
+*/
+
+namespace MyServe
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var uri = "http://localhost:9999";
+			Console.WriteLine (uri);
+			var host = new NancyHost (new Uri(uri));
+			Console.WriteLine (host);
+			host.Start ();
+
+			if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+			{
+				Thread.Sleep(Timeout.Infinite);
+			}
+			else
+			{
+				Console.ReadKey();
+			}
+
+			host.Stop();  // stop hosting
+
+			Console.WriteLine ("Hello World!@");
+		}
+	}
+}
---
+++ myWebServe/boDemo.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace businessLogic
+{
+	public class boDemo:NancyModule
+	{
+		public boDemo ()
+		{
+			Get ["/"] = _ => "Hello World";
+
+			Get ["/test"] = _ => "Hello World#2";
+		}
+	}
+}
+
