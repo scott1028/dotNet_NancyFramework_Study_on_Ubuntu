95c3cfd5ed95ae0f5c51e5fbe0158147b281e523
scott.lan <scott.lan@taisys.com>
2015-04-13 18:37:20 +0800

    add prototype extension sample

--- myWebServe/Program.cs
+++ myWebServe/Program.cs
@@ -11,9 +11,37 @@ using System.Linq;
  * ref: https://github.com/NancyFx/Nancy/wiki/Defining-routes#route-segment-constraints
  * ref: http://stackoverflow.com/questions/15350552/how-to-get-real-error-messages-in-nancy-on-mono
 */
+namespace CustomExtensions
+{
+	//Extension methods must be defined in a static class 
+	public static class StringExtension
+	{
+		// This is the extension method. 
+		// The first parameter takes the "this" modifier
+		// and specifies the type for which the method is defined. 
+		public static void toEcho2(this String str)
+		{
+			Console.WriteLine ("extend Method #2");
+		}
+	}
+}
 
 namespace MyServe
 {
+	// mix-in extension method
+	// 必須寫於 Namespace 最上方
+	using CustomExtensions;
+	using myExtension;
+
+	// MonkeyPatch & Prototype Extends
+	public static class method
+	{
+		public static void toEcho(this String str){
+			Console.WriteLine ("String.toEcho() extension method");
+		}
+	}
+		
+	// Main
 	class MainClass
 	{
 		public static void Main (string[] args)
@@ -21,6 +49,10 @@ namespace MyServe
 			var uri = "http://localhost:9999";
 			Console.WriteLine (uri);
 			String test = "123123";
+			test.toEcho ();			// extend from Program.cs
+			test.toEcho2 ();		// extend from Program.cs
+			test.toEcho3 ();		// extend from myExtension.cs
+
 			Console.WriteLine (test);
 			var host = new NancyHost (new Uri(uri));
 			Console.WriteLine (host);
---
+++ myWebServe/myExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myExtension
+{
+//	public class myExtension
+//	{
+//		public myExtension ()
+//		{
+//		}
+//	}
+	//Extension methods must be defined in a static class 
+	public static class StringExtension
+	{
+		// This is the extension method. 
+		// The first parameter takes the "this" modifier
+		// and specifies the type for which the method is defined. 
+		public static void toEcho3(this String str)
+		{
+			Console.WriteLine ("extend Method #3");
+		}
+	}
+}
+
